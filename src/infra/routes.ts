37c71bd1ec77ceb02a774e677eef1019f067e2de
carlos henrique <carlos.andrade.henrique1@outlook.com>
2021-07-09 15:11:41 -0300

    fix: remove user create route

--- src/infra/routes.ts
+++ src/infra/routes.ts
@@ -1,24 +1,22 @@
 import { AuthController } from '@controllers/AuthController'
-import { UserController } from '@controllers/UserController'
 import { PostController } from '@controllers/PostController'
 import { Auth } from '@middlewares/auth'
 import { Router } from 'express'
 const routes = Router()
 
 // instanciando controllers
-const userController = new UserController()
 const authController = new AuthController()
 const postController = new PostController()
 // rota de criacao de usuario
-routes.post('/users', userController.create)
+// routes.post('/users', userController.create)
 // rota de autenticaçao
 routes.post('/authentication', authController.authUser)
 // rota de criacao de posts
 routes.post('/posts', Auth, postController.create)
 // rota de listagem de posts
-routes.get('/posts', Auth, postController.index)
+routes.get('/posts', postController.index)
 // rota de buscar somente um post
-routes.get('/posts/:slug', Auth, postController.show)
+routes.get('/posts/:slug', postController.show)
 // rota para deletar um post
 routes.delete('/posts/:id', Auth, postController.del)
 // rota de edicao de um post
